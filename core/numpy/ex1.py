f0114a8dc2916f5e985970067e5cf39a561053a6
Jalilov <javohirjaliloff@gmail.com>
2023-02-08 23:03:24 +0500

    add emoji

--- core/numpy/ex1.py
+++ core/numpy/ex1.py
@@ -4,16 +4,16 @@ import numpy as np
 class Array1D:
     def check(self, solution):
         if len(solution.shape) == 1 and solution.__class__ == np.ndarray:
-            print("Accepted")
+            print("✅Accepted")
         else:
-            print("Incorrect, solution is not a 1D array")
+            print("❌Incorrect, solution is not a 1D array")
 
 class ArrayZerosNxM:
     def check(self, solution):
         if solution.__class__ == np.ndarray and (solution == 0).all() == True and len(solution.shape) == 2:
-            print("Accepted")
+            print("✅Accepted")
         else:
-            print("Incorrect, solution is not a NxM array of zeros")
+            print("❌Incorrect, solution is not a NxM array of zeros")
 
 q0 = Array1D()
 q1 = ArrayZerosNxM()
