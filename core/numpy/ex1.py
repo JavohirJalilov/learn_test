eb3dd0a8dfe70f9bc934eacdb5b4f86e86b8d325
Jalilov <javohirjaliloff@gmail.com>
2023-02-08 22:53:26 +0500

    added tasks

--- core/numpy/ex1.py
+++ core/numpy/ex1.py
@@ -7,5 +7,13 @@ class Array1D:
             print("Accepted")
         else:
             print("Incorrect, solution is not a 1D array")
-        
+
+class ArrayZerosNxM:
+    def check(self, solution):
+        if solution.__class__ == np.ndarray and (solution == 0).all() == True and len(solution.shape) == 2:
+            print("Accepted")
+        else:
+            print("Incorrect, solution is not a NxM array of zeros")
+
 q0 = Array1D()
+q1 = ArrayZerosNxM()
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+from core.numpy.ex1 import *
+
+q0.check(np.array([1, 2, 3]))
+q1.check(np.zeros((2, 3, 1)))
